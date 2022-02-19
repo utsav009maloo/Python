bbae562b88c3d4b38011a8a86daa5ebe27e14b69
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2022-02-19 12:54:43 +0530

    Python Assignment DAY 3
    Day 3 Assignments Submission

---
+++ DAY 3 Assignment/11 and 12 Answers.py
@@ -0,0 +1,7 @@
+#Create a dictionary numbers = {'one':1, 'two':2, 'three':3}. Pull out the number '2' by calling the key 'two'
+my_dict = {'one':1, 'two':2, 'three':3}
+print(my_dict['two'])
+
+#Create a tuple with the numbers 8, 9, and 10?
+tuplex = 8, 9, 10
+print(tuplex)
