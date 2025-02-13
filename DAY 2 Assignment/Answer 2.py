69a251d97a59b909cb957cb95f9626b22de0679d
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-02-13 11:36:12 +0530

    .

--- DAY 2 Assignment/Answer 2.py
+++
@@ -1,8 +0,0 @@
-#Write the syntax of a simple if statement.
-num = 10
-if num > 0:
-   print("Positive Number")
-elif num == 0:
-   print("ZERO")
-else:
-   print("Negative Number")
--- DAY 2 Assignment/Answer 5.py
+++
@@ -1,7 +0,0 @@
-#Write a program to check whether a number entered by user is even or odd.
-
-A = int(input("Enter the number: -"))
-if (A % 2) == 0:
-	print("No. is EVEN")
-else:
-	print("No. is Odd")
