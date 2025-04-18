0967f54af8df194cbefe00766cfb057b4aa6518a
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-04-18 18:37:32 +0530

    ..

--- Keyword.py
+++ Keyword.py
@@ -3,7 +3,28 @@
 #print (5 > 5)
 
 #break
-for i in range(1,11): 
-    if i == 5: 
-        break
-    print(i) 
+# for i in range(1,11): 
+#     if i == 5: 
+#         break
+#     print(i) 
+
+#continue
+# for i in range(1,15):
+#     if i == 5:
+#         continue
+#     print(i)
+# 
+
+# EF ELIF ELSE
+# num = 2
+# if num == 1:
+#     print("One")
+# elif num == 2:
+#     print("two")
+# else:
+#     print("BYE")
+
+
+# 
+
+
