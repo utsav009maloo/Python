79370e72d0151c83576b5203a2b382811710d12c
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-04-23 17:03:31 +0530

    python basic

---
+++ PYTHON 1 BASIC/dic.py
@@ -0,0 +1,19 @@
+# import os
+# print(os.getcwd())
+
+# from math import pi
+# print(pi)
+
+# Python DICTIONARY is thre unorder collection of items
+
+new_dict = {1:"hello",2:"world",3:"UTSAV"}
+print(new_dict)
+print(new_dict[1])
+print(new_dict.get(3))
+
+new_dict[1] = "HEY"
+new_dict[4] = "Namaste"
+print(new_dict)
+
+# new_dict.clear()
+# print(new_dict)
