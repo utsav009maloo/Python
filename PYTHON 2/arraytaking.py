bd18835797c02a6be9faabf7e149140b16b00dea
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-05-27 15:33:12 +0530

    ..

--- PYTHON 2/arraytaking.py
+++ PYTHON 2/arraytaking.py
@@ -4,3 +4,12 @@ for i in range(0, x):
     ele = input()
     lst.append(ele)
     print(lst)
+
+
+#Sorting in array
+for k in range(0,len(lst)):
+    for l in range(k + 1 , len(lst)):
+        if lst[k] >= lst[l]:
+            lst[k],lst[l] = lst[l],lst[k]
+
+print(lst)
