9753e9de7ee6b5a2709dabcebe9db473b3aabf59
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-06-23 18:32:19 +0530

    .

--- PYTHON 1 BASIC/functons.py
+++ PYTHON 1 BASIC/functons.py
@@ -1,2 +1,14 @@
-a = 2
-print(id(a))
+# a = 2
+# print(id(a))
+s = "GeeksforGeeks"
+
+# Initialize an empty string to hold reversed result
+rev = ""
+
+# Loop through each character in original string
+for ch in s:
+  
+    # Add current character to front of reversed string
+    rev = ch + rev
+
+print(rev)
